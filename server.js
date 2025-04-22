440bcabb41240ad7a72296558300812c61ade7a5
GH-2018-prod <maancuza@gmail.com>
2025-04-22 07:08:18 -0600

    uploading front end

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
@@ -16,12 +17,22 @@ app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
 
+//public directory
+app.use(express.static( 'public' ))
+
 // Rutas
 app.use('/api/auth', require('./routes/authRoutes'));
+
+//Public route
+app.use('*',(req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 app.get('/', (req, res) => {
   res.send('API funcionando 🚀')
 });
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`))
