1a098660a11c2776f66a59541dbd46199bf21474
GH-2018-prod <maancuza@gmail.com>
2026-04-22 05:30:55 -0600

    adding weekly filter selection in galery

--- routes/link.js
+++ routes/link.js
@@ -8,32 +8,17 @@ const { auth } = require('../middleware/authMiddleware');
 router.get('/link/:key', auth, async (req, res) => {
   try {
     const { key } = req.params;
-
-    // let query = {
-    //   key: key.toUpperCase(),
-    //   active: true
-    // };
-
-    // // 👤 Si NO es admin → solo link activo (ya cubierto arriba)
-    // // 👑 Si es admin → podría ver incluso inactivos
-    // if (req.user.role === 'admin') {
-
-    //   delete query.active; // admin ve todos
-    // }
     const isAdmin = req.user.role === 'admin'
     const query = {
       key: key.toUpperCase(),
       ...( isAdmin ? {} : { active: true })
     }
-
     const link = await Link.findOne(query);
 
     if (!link) {
       return res.status(404).json({ message: 'Link no encontrado' });
     }
-
     res.json(link);
-
   } catch (error) {
     res.status(500).json({ message: 'Error obteniendo link' });
   }
